407f454db9c030c1708a28dd054e8d2624b6929a
Atinder-Pal <atty.pal46@gmail.com>
2021-05-20 16:19:37 -0600

    Added links to buttons in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,31 @@
 		<v-app-bar app color="primary" dark>
 			<v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
 			<v-spacer></v-spacer>
-			<v-btn text rounded>Home</v-btn>
-			<v-btn text rounded>Login</v-btn>
+			<v-btn
+				v-for="link in links"
+				:key="link.label"
+				text
+				rounded
+				:to="link.url"
+				>{{ link.label }}</v-btn
+			>
 		</v-app-bar>
-		<v-content> </v-content>
+		<v-content>
+			<router-view></router-view>
+		</v-content>
 
 		<v-footer color="primary lighten-1" padless>
 			<v-row justify="center" no-gutters>
 				<v-btn
 					v-for="link in links"
-					:key="link"
+					:key="link.label"
 					color="white"
 					text
 					rounded
 					class="my-2"
+					:to="link.url"
 				>
-					{{ link }}
+					{{ link.label }}
 				</v-btn>
 				<v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
 					{{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
@@ -39,7 +48,16 @@
 		},
 
 		data: () => ({
-			links: ['Home', 'Login'],
+			links: [
+				{
+					label: 'Home',
+					url: '/',
+				},
+				{
+					label: 'Login',
+					url: '/login',
+				},
+			],
 		}),
 	};
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -22,7 +21,7 @@ const routes = [
 	},
 	{
 		path: '/login',
-		name: 'login',
+		name: 'Login',
 		component: () => import('../views/Login.vue'),
 	},
 ];
