0d2f9b0b7e7c19fef7b313924a72ee3cffaee05c
Atinder-Pal <atty.pal46@gmail.com>
2021-05-20 13:56:43 -0600

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,50 @@
 			<v-btn>Home</v-btn>
 			<v-btn>Login</v-btn>
 		</v-app-bar>
-		<!-- Login Module -->
-		<v-card width="400" class="mx-auto mt-5">
-			<v-card-title class="pb-5">
-				<h1 class="display-1">Login</h1>
-			</v-card-title>
-			<v-card-text>
-				<v-form>
-					<v-text-field label="Username" prepend-icon="mdi-account-circle" />
-					<v-text-field
-						label="Password"
-						:type="showPassword ? 'text' : 'password'"
-						prepend-icon="mdi-lock"
-						:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-						@click:append="showPassword = !showPassword"
-					/>
-				</v-form>
-			</v-card-text>
-			<v-divider></v-divider>
-			<v-card-actions>
-				<v-btn color="success">Register</v-btn>
-				<v-spacer></v-spacer>
-				<v-btn color="info">Login</v-btn>
-			</v-card-actions>
-		</v-card>
+		<v-content>
+			<!-- Login Module -->
+			<v-card width="400" class="mx-auto mt-5">
+				<v-card-title class="pb-5">
+					<h1 class="display-1">Login</h1>
+				</v-card-title>
+				<v-card-text>
+					<v-form>
+						<v-text-field label="Username" prepend-icon="mdi-account-circle" />
+						<v-text-field
+							label="Password"
+							:type="showPassword ? 'text' : 'password'"
+							prepend-icon="mdi-lock"
+							:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+							@click:append="showPassword = !showPassword"
+						/>
+					</v-form>
+				</v-card-text>
+				<v-divider></v-divider>
+				<v-card-actions>
+					<v-btn color="success">Register</v-btn>
+					<v-spacer></v-spacer>
+					<v-btn color="info">Login</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-content>
+
+		<v-footer color="primary lighten-1" padless>
+			<v-row justify="center" no-gutters>
+				<v-btn
+					v-for="link in links"
+					:key="link"
+					color="white"
+					text
+					rounded
+					class="my-2"
+				>
+					{{ link }}
+				</v-btn>
+				<v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+					{{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+				</v-col>
+			</v-row>
+		</v-footer>
 	</v-app>
 </template>
 
@@ -45,6 +65,7 @@
 
 		data: () => ({
 			showPassword: false,
+			links: ['Home', 'Login'],
 		}),
 	};
 </script>
